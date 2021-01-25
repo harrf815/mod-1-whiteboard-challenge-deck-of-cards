d2db9362b05458664ddd2036bfb9de19bb845b0b
Harrf Akbar <harrf_akbar@yahoo.com>
2021-01-25 10:09:44 -0800

    commit tree 1a48d8dfd7458bbbc348d2faf4a5911838b406eb

--- solution.rb
+++ solution.rb
@@ -1,7 +1,35 @@
+require 'pry'
 class Deck
+
+    attr_reader :suits, :ranks, :cards 
+
+    def initialize
+        @cards = []
+        ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        suits = ['hearts', 'diamonds', 'clubs', 'spades']
+        
+        ranks.each do |rank|
+            suits.each do |suit|
+                @cards << Card.new(suit, rank)
+            #  binding.pry  
+            end
+        end
+
+    end
+
+    def choose_card
+
+    end
 
 end
 
 class Card
+
+    attr_accessor :rank, :suit
+
+    def initialize(suit, rank)
+        @rank = rank
+        @suit = suit 
+    end
 
 end
